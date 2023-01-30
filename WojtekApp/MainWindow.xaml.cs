08b32812fc7caf62e19901f37ae22d03f8bcd391
Filip Pawelec <fpawelec0@gmail.com>
2023-01-30 16:08:33 +0100

    Add project files.

---
+++ WojtekApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WojtekApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StudentDAO StudentDAO = new("Server=(localdb)\\MSSQLLocalDB;Integrated Security=True;Database=StudentsDatabase");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            Student student= new Student();
+            student.Name = nameTextbox.Text;
+            student.Surname = surnameTextbox.Text;
+            student.Class = classTextbox.Text;
+            
+            StudentDAO.create(student);
+        }
+
+        private void showButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<Student> students = StudentDAO.getAll();
+
+            showDatagrid.ItemsSource= students;
+        }
+    }
+}
---
+++ WojtekApp/StudentDAO.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace WojtekApp
+{
+    public class StudentDAO
+    {
+        public string connString { get; set; }
+
+        public StudentDAO(string connectionString)
+        {
+            connString = connectionString;
+        }
+
+        public void create(Student student)
+        {
+
+            SqlConnection con = new SqlConnection(connString);
+
+            String query = $"INSERT INTO StudentsData (Name, Surname, Class) VALUES ('{student.Name}', '{student.Surname}', '{student.Class}')";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            SqlDataReader dbr;
+            try
+            {
+                con.Open();
+                dbr = cmd.ExecuteReader();
+                while (dbr.Read())
+                {
+                }
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.Message);
+            }
+        }
+
+        public List<Student> getAll()
+        {
+            List<Student> students = new();
+            SqlConnection con = new SqlConnection(connString);
+
+            String query = $"SELECT * FROM StudentsData";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            SqlDataReader dbr;
+            try
+            {
+                con.Open();
+                dbr = cmd.ExecuteReader();
+                while (dbr.Read())
+                {
+                    string sID = dbr["Id"].ToString();
+                    string sname = (string)dbr["Name"]; // name is string value
+                    string ssurname = (string)dbr["Surname"];
+                    string sclass = dbr["Class"].ToString();
+                    Student student = new Student
+                    {
+                        Id = int.Parse(sID),
+                        Name = sname,
+                        Surname = ssurname,
+                        Class = sclass
+                    };
+                    students.Add(student);
+                }
+            }
+            catch (Exception es)
+            {
+                MessageBox.Show(es.Message);
+            }
+            return students;
+        }
+    }
+}
